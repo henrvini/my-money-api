773c04a3e4f28d00778341a1ba36e4ed8274f3c1
Vinicius Rodrigues <vini0593@gmail.com>
2023-04-25 16:06:55 -0300

    adding protected and opened routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const auth = require("./auth");
 
 module.exports = (server) => {
-  const router = express.Router();
-  server.use("/api", router);
+  // ROTAS PROTEGIDAS POR TOKEN JWT
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
+
+  protectedApi.use(auth);
 
   const BillingCycle = require("../api/billingCycle/billingCycleService");
-  BillingCycle.register(router, "/billingCycles");
+  BillingCycle.register(protectedApi, "/billingCycles");
+
+  // ROTAS ABERTAS
+  const openApi = express.Router();
+  server.use("/oapi", openApi);
+
+  const AuthService = require("../api/user/AuthService");
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signup", AuthService.signup);
+  openApi.post("/validateToken", AuthService.validateToken);
 };
